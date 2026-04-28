e95d6e0a6ef49f3451683c9b568c1065609fcb0a
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-28 14:10:54 +0300

    Step 4: HeroesStore and HeroesController

---
+++ HeroesApi/Data/HeroesStore.cs
@@ -0,0 +1,88 @@
+
+using HeroesApi.Models;
+
+namespace HeroesApi.Data;
+
+public static class HeroesStore
+{
+    public static List<Hero> Heroes { get; } = new()
+    {
+        new Hero
+        {
+            Id         = 1,
+            Name       = "Человек-паук",
+            RealName   = "Питер Паркер",
+            Universe   = Universe.Marvel,
+            PowerLevel = 75,
+            Powers     = new() { "паутина", "дазание по стенам", "паучье чутье" },
+            Weapon     = new() { Name = "Паутинострел", IsRanged = true },
+            InternalNotes = "Любимый герой редактор"
+        },
+        new Hero
+        {
+            Id         = 2,
+            Name       = "Бэтмен",
+            RealName   = "Брюс Уэйн",
+            Universe   = Universe.DC,
+            PowerLevel = 70,
+            Powers     = new() { "интеллект", "боевые искусства", "технологии" },
+            Weapon     = new() { Name = "Бэтаранг", IsRanged = true },
+            InternalNotes = "Без суперсил, только деньги и упорство"
+        },
+        new Hero
+        {
+            Id         = 3,
+            Name       = "Железный человек",
+            RealName   = "Тони Старк",
+            Universe   = Universe.Marvel,
+            PowerLevel = 85,
+            Powers     = new() { "броня", "полет", "интелект", "лазеры" },
+            Weapon     = new() { Name = "Костюм марк 50", IsRanged = true },
+            InternalNotes = "Я - Железный человек"
+        },
+        new Hero
+        {
+            Id         = 4,
+            Name       = "Грут",
+            RealName   = "Грут",
+            Universe   = Universe.Marvel,
+            PowerLevel = 80,
+            Powers     = new() { "регенерация", "управление деревом", "суперсила" },
+            Weapon     = new() { Name = "Ветви", IsRanged = false },
+            InternalNotes = "Я есть Грут"
+        },
+        new Hero
+        {
+            Id         = 5,
+            Name       = "Тор",
+            RealName   = "Тор Одинсон",
+            Universe   = Universe.Marvel,
+            PowerLevel = 95,
+            Powers     = new() { "молния", "полет", "суперсила" },
+            Weapon     = new() { Name = "Мьёльнир", IsRanged = false },
+            InternalNotes = "Бог грома"
+        },
+        new Hero
+        {
+            Id         = 6,
+            Name       = "Росомаха",
+            RealName   = "Логан",
+            Universe   = Universe.Marvel,
+            PowerLevel = 85,
+            Powers     = new() { "регенерация", "когти", "замедленное старение" },
+            Weapon     = new() { Name = "Адамантиевые когти", IsRanged = false },
+            InternalNotes = "Лучший у меня есть"
+        },
+        new Hero
+        {
+            Id         = 7,
+            Name       = "Дэдпул",
+            RealName   = "Уэйд Уилсон",
+            Universe   = Universe.Marvel,
+            PowerLevel = 80,
+            Powers     = new() { "регенераця", "владение оружием", "болтовня" },
+            Weapon     = new() { Name = "Катаны и пистолеты", IsRanged = true },
+            InternalNotes = "Разрушает четвертую стену"
+        },
+    };
+}

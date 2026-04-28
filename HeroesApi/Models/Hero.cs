14dfc7df3131fd7da72abf0515e92f2e1b7134fe
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-04-28 13:12:02 +0300

    Step 3: JSON configured - camelCase, enum as string
    git push

--- HeroesApi/Models/Hero.cs
+++ HeroesApi/Models/Hero.cs
@@ -10,17 +10,18 @@ public class Weapon
         Marvel,
         DC
     }
-}
-public class Hero
-{
-    public int Id { get; set; }
-    [JsonPropertyName("name")]
-    public string Name { get; set; } = string.Empty;
-    public string RealName { get; set; } = string.Empty;
-    public Universe Universe { get; set; }
-    public int PowerLevel { get; set; }
-    public List<string> Powers { get; set; } = new();
-    public Weapon Weapon { get; set; } = new();
-    [JsonIgnore]
-    public string? InternalNotes { get; set; }
+
+    public class Hero
+    {
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+        public string RealName { get; set; } = string.Empty;
+        public Universe Universe { get; set; }
+        public int PowerLevel { get; set; }
+        public List<string> Powers { get; set; } = new();
+        public Weapon Weapon { get; set; } = new();
+        [JsonIgnore]
+        public string? InternalNotes { get; set; }
+    }
 }
